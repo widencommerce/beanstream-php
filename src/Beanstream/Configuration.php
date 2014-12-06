c05e2c89ba943e822b2358a14b27fedbb7825a8a
Kevin Saliba <ksaliba@beanstream.com>
2014-12-05 23:04:30 -0800

    syncing up endpoints and config for AS

---
+++ src/Beanstream/Configuration.php
@@ -0,0 +1,64 @@
+<?php
+namespace Beanstream;
+
+
+
+
+
+class Configuration {
+
+    /**
+     * Configuration: API Version
+     * 
+     * @var string $_version
+     */
+	protected $_version = 'v1';
+
+    /**
+     * Configuration: API Platform
+     * 
+     * @var string $_platform
+     */
+	protected $_version = 'www';
+
+
+    /**
+     * Configuration: Merchant ID
+     * 
+     * @var int $_merchantId
+     */
+	protected $_merchantId;
+
+    /**
+     * Configuration: Payments API Passcode
+     * 
+     * @var string $_paymentsApiPasscode
+     */
+	protected $_paymentsApiPasscode;
+
+    /**
+     * Configuration: Profiles API Passcode
+     * 
+     * @var string $_profilesApiPasscode
+     */
+	protected $_profilesApiPasscode;
+
+    /**
+     * Configuration: Reporting API Passcode
+     * 
+     * @var string $_reportingApiPasscode
+     */
+	protected $_reportingApiPasscode;
+
+
+
+
+
+
+
+
+
+
+
+}
+
