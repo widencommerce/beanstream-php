f39a8677270ce7bf92fb9a9c89fc5372f3775e83
Kevin Saliba <ksaliba@beanstream.com>
2015-01-01 10:11:45 -0800

    added phpdocs

--- src/Beanstream/Configuration.php
+++ src/Beanstream/Configuration.php
@@ -1,7 +1,7 @@
 <?php	namespace Beanstream;
 
 /**
- * Configuration class to handle merchant id, api keys, platform & version defaults 
+ * Configuration class to handle merchant id, api keys, platform & version 
  *  
  * @author Kevin Saliba
  */
@@ -25,64 +25,121 @@ class Configuration {
     /**
      * Configuration: Merchant ID
      * 
-     * @var int $_merchantId
+     * @var string $_merchantId
      */
 	protected $_merchantId;
 
 
     /**
-     * Configuration: API Key
+     * Configuration: API Keys
      * 
-     * @var array $_apiKeys
+     * @var string[] $_apiKeys
      */
 	protected $_apiKeys;
 
 
 
 
-
+	/**
+	 * setMerchantId() function
+	 *
+	 * @param string $merchantId
+	 * @return void
+	 */
 	public function setMerchantId($merchantId = '') {
-		if (strlen($merchantId) !== 9) { //switch to actual real assertmerchantId
+		//check to make sure we have a 9 digit string containing only digits 0-9
+		if (!preg_match('/^[0-9]{9}$/', $merchantId)) { //TODO switch to actual real assertmerchantId
 			//throw exception
-			
+			throw new ConfigurationException('Invalid Merchant ID provided: '.$merchantId. ' Expected 9 digits.');
 		}
 		$this->_merchantId = $merchantId;
 	}
 
+	/**
+	 * getMerchantId() function
+	 *
+	 * @return string merchant id
+	 */
 	public function getMerchantId() {
 		return $this->_merchantId;
 	}
 
 
-
-	public function setApiKeys($apiKeys = '') {
+	/**
+	 * setApiKeys() function
+	 * 
+	 * Takes in an array of keys 'payments', 'profiles', 'reporting'
+	 * and string values for each key. Not every key is needed, only
+	 * those being used/set in Beanstream Dashboard > Order Settings.
+	 *
+	 * @param string[] $apiKeys
+	 * @return void
+	 */
+	public function setApiKeys($apiKeys = array()) {
 		$this->_apiKeys=$apiKeys;
 	}
+	/**
+	 * getApiKey() function
+	 *
+	 * @param string $key which key to return: 'payments', 'profiles', 'reporting' 
+	 * @return string api key
+	 */
 	public function getApiKey($key) {
 		return $this->_apiKeys[$key];
 	}
 
 
 
-
+	/**
+	 * setPlatform() function
+	 * 
+	 * @param string[] $platform
+	 * @return void
+	 */
 	public function setPlatform($platform = '') {
-		if (strlen($platform) > 0) { //switch to actual real assertnotempty
+		//make sure it's not blank
+		//if blank, don't set it and use default declared above
+				if (strlen($platform) > 0) { //TODO switch to actual real assertnotempty
 			$this->_platform=$platform;
 		}
 	}
+	
+	/**
+	 * getPlatform() function
+	 *
+	 * @return string platform
+	 */
 	public function getPlatform() {
 		return $this->_platform;
 	}	
 	
 	
-
+	/**
+	 * setApiVersion() function
+	 * 
+	 * @param string[] $version
+	 * @return void
+	 */
 	public function setApiVersion($version = '') {
-		if (strlen($version) > 0) { //switch to actual real assertnotempty
+		//make sure it's not blank
+		//if blank, don't set it and use default declared above
+		if (strlen($version) > 0) { //TODO switch to actual real assertnotempty
 			$this->_version=$version;
 		}
 	}
+
+	/**
+	 * getApiVersion() function
+	 *
+	 * @return string version
+	 */
 	public function getApiVersion() {
 		return $this->_version;
 	}	
+	
+	
+	
+	
+	
 }
 
--- src/Beanstream/Gateway.php
+++ src/Beanstream/Gateway.php
@@ -11,40 +11,41 @@ require_once 'communications/HttpConnector.php';
 
 
 /**
- * Main class to facilitate comms with Beanstream gateway,
+ * Main class to facilitate comms with Beanstream Gateway,
  *  
  * @author Kevin Saliba
  */
-class Gateway
-{
+class Gateway {
 
-
-	
-	/**
-     * Auth string
-     * 
-     * @var string
-     */
-    protected $_auth;
-    
 	/**
      * Config object
+	 * 
+	 * Holds mid, apikeys[], platform, api version
      * 
-     * @var string
+     * @var	\Beanstream\Configuration	$_config
      */
     protected $_config;
     
+	
+	/**
+	 * API Objects
+	 * 
+	 * Holds API objects with appropriate config
+	 * 
+	 * @var	\Beanstream\Payments		$_paymentsAPI
+	 * @var	\Beanstream\Profiles		$_profilesAPI
+	 * @var	\Beanstream\Reporting 	$_reportingAPI
+	 */
+	protected $_paymentsAPI; 
+	protected $_profilesAPI; 
+	protected $_reportingAPI; 
 
-
-	protected $paymentsAPI; 
-	protected $profilesAPI; 
-	protected $reportingAPI; 
 
     /**
      * Constructor
      * 
      * @param string $merchantId Merchant ID
-     * @param array $apiKeys API Access Passcodes 
+     * @param string[] $apiKeys API Access Passcodes array
      * @param string $platform API Platform (default 'www')
      * @param string $version API Version (default 'v1')
      */
@@ -57,328 +58,102 @@ class Gateway
 		$this->_config->setApiVersion($version);
     }
 	
+
+	/**
+	 * getConfig() function
+	 *
+	 * @return \Beanstream\Configuration this gateway's set config
+	 */
 	public function getConfig() {
 		return $this->_config;
 	}
-
-	public function getMerchantId() {
-		return $this->_config->getMerchantId();
-	}
-		
-		
+	
+	
+	/**
+	 * payments() function.
+	 * 
+	 * Public facing function to return the configured payment API
+	 * All comms with the Payments API will go through this function
+	 *
+	 * @return \Beanstream\Payments this gateway's payment api object
+	 */	
 	public function payments() {
 		return $this->getPaymentsApi();
 	}	
 
+	/**
+	 * getPaymentsApi() function.
+	 * 
+	 * Internal function to return/create the payment object
+	 *
+	 * @return \Beanstream\Payments this gateway's payment api object, creates it if it doesn't exist
+	 */
 	private function getPaymentsApi() {
-		if (is_null($this->paymentsAPI)) {
-			$this->paymentsAPI = new Payments($this->_config);
+		//check to see if we already have it created 
+		if (is_null($this->_paymentsAPI)) {
+			//if we don't, create it
+			$this->_paymentsAPI = new Payments($this->_config);
 		}
-		return $this->paymentsAPI;
+		return $this->_paymentsAPI;
 	}
 	
-		
+
+	/**
+	 * profiles() function.
+	 * 
+	 * Public facing function to return the configured profiles API
+	 * All comms with the Profiles API will go through this function
+	 *
+	 * @return \Beanstream\Profiles this gateway's profiles api object
+	 */	
 	public function profiles() {
 		return $this->getProfilesApi();
-	}	
-		
-	private function getProfilesApi() {
-		if (is_null($this->profilesAPI)) {
-			$this->profilesAPI = new Profiles($this->_config);
+	}
+
+	/**
+	 * getProfilesApi() function.
+	 * 
+	 * Internal function to return/create the profiles object
+	 *
+	 * @return \Beanstream\Profiles this gateway's profiles api object, creates it if it doesn't exist
+	 */
+	 private function getProfilesApi() {
+		//check to see if we already have it created 
+		if (is_null($this->_profilesAPI)) {
+			//if we don't, create it
+			$this->_profilesAPI = new Profiles($this->_config);
 		}
-		return $this->profilesAPI;
+		return $this->_profilesAPI;
 	}
 
 
+	/**
+	 * reporting() function.
+	 * 
+	 * Public facing function to return the configured reporting API
+	 * All comms with the Reporting API will go through this function
+	 *
+	 * @return \Beanstream\Reporting this gateway's reporting api object
+	 */	
 	public function reporting() {
 		return $this->getReportingApi();
 	}	
-		
+
+	/**
+	 * getReportingApi() function.
+	 * 
+	 * Internal function to return/create the reporting object
+	 *
+	 * @return \Beanstream\Reporting this gateway's reporting api object, creates it if it doesn't exist
+	 */
 	private function getReportingApi() {
-		if (is_null($this->reportingAPI)) {
-			$this->reportingAPI = new Reporting($this->_config);
+		//check to see if we already have it created 
+		if (is_null($this->_reportingAPI)) {
+			//if we don't, create it
+			$this->_reportingAPI = new Reporting($this->_config);
 		}
-		return $this->reportingAPI;
+		return $this->_reportingAPI;
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-    
-    /**
-     * Purchases and pre-authorization
-     * @link http://developer.beanstream.com/documentation/take-payments/purchases/card/
-     * @link http://developer.beanstream.com/documentation/legato/take-payment-legato-token/
-     * @link http://developer.beanstream.com/documentation/take-payments/purchases/cash/
-     * @link http://developer.beanstream.com/documentation/take-payments/purchases/cheque-purchases/
-     * 
-     * @param array Payment data, by default 'payment_method' is 'card'
-     * @return array Transaction details
-     */
-    public function payment($data)
-    {
-        $res = $this->request('payments', $data + array(
-            'payment_method' => 'card',
-        ), 'POST');
-        
-        return $res;
-    }
-    
-    /**
-     * Pre-authorization completion
-     * @link http://developer.beanstream.com/documentation/take-payments/pre-authorization-completion/
-     * 
-     * @param string $oid Transaction Id
-     * @param mixed $amount Order amount
-     * @param string[optional] $order_number
-     * @return array Transaction details
-     */
-    public function complete($tid, $amount, $order_number = null)
-    {
-        $data = array('amount' => $amount);
-        if ( ! is_null($order_number)) {
-            $data['order_number'] = $order_number;
-        }
-        $res = $this->request('payments/'.$tid.'/completions', $data, 'POST');
-        
-        return $res;
-    }
-    
-    /**
-     * Void (aka cancel)
-     * @link http://developer.beanstream.com/documentation/take-payments/voids/
-     * 
-     * @param string $tid Transaction Id
-     * @param mixed $amount Order amount
-     * @return array Transaction details
-     */
-    public function void($tid, $amount)
-    {
-        $res = $this->request('payments/'.$tid.'/void', array('amount' => $amount), 'POST');
-        
-        return $res;
-    }
-    
-    /**
-     * Return (aka refund)
-     * @link http://developer.beanstream.com/documentation/take-payments/return/
-     * 
-     * @param string $oid Transaction Id
-     * @param mixed $amount Order amount
-     * @param string[optional] $order_number
-     * @return array Transaction details
-     */
-    public function refund($tid, $amount, $order_number = null)
-    {
-        $data = array('amount' => $amount);
-        if ( ! is_null($order_number)) {
-            $data['order_number'] = $order_number;
-        }
-        $res = $this->request('payments/'.$tid.'/returns', $data, 'POST');
-        
-        return $res;
-    }
-    
-    /**
-     * Take payment - profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/take-payment-profiles/
-     * 
-     * @param string $pid Profile Id
-     * @param int $cid Card Id
-     * @param array $data Order data
-     * @param bool[optional] $complete Set to false for pre-auth
-     * @return array Transaction details
-     */
-    public function paymentProfile($pid, $cid, $data, $complete = true)
-    {
-        $res = $this->request('payments', $data + array(
-            'payment_method' => 'payment_profile',
-            'payment_profile' => array(
-                'complete' => $complete,
-                'customer_code' => $pid,
-                'card_id' => $cid,
-            )
-        ), 'POST');
-        
-        return $res;
-    }
-    
-    /**
-     * Create a new profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/create-new-profile/
-     * 
-     * @param array $data Profile data
-     * @return string Profile Id (aka customer_code)
-     */
-    public function createProfile($data)
-    {
-        $res = $this->request('profiles', $data, 'POST');
-        
-        return $res['customer_code'];
-    }
-    
-    /**
-     * Retrieve a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/retrieve-profile/
-     * 
-     * @param string $pid Profile Id
-     * @return array Profile data
-     */
-    public function retrieveProfile($pid)
-    {
-        $res = $this->request('profiles/'.$pid);
-        unset($res['code'], $res['message']);
-        
-        return $res;
-    }
-    
-    /**
-     * Update a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/update-profile/
-     * 
-     * @param string $pid Profile Id
-     * @param array $data Profile data
-     * @return TRUE
-     */
-    public function updateProfile($pid, $data)
-    {
-        $this->request('profiles/'.$pid, $data, 'PUT');
-        
-        return true;
-    }
-    
-    /**
-     * Delete a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/delete-profile/
-     * 
-     * @param string $id Profile Id
-     * @return TRUE
-     */
-    public function deleteProfile($pid)
-    {
-        $this->request('profiles/'.$pid, null, 'DELETE');
-        
-        return true;
-    }
-    
-    /**
-     * Add card to a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/add-card-profile/
-     * 
-     * @param string $pid Profile Id
-     * @param array $data Card data
-     * @return TRUE
-     */
-    public function addCard($pid, $data)
-    {
-        $this->request('profiles/'.$pid.'/cards', array('card' => $data), 'POST');
-        
-        /*
-         * XXX it would be more appropriate to return newly added card id,
-         * but Beanstream for some reason does not return it in result
-         */
-        return true;
-    }
-    
-    /**
-     * Retrieve cards in a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/retrieve-cards-profile/
-     * 
-     * @param string $pid Profile Id
-     * @return array Cards data
-     */
-    public function retrieveCards($pid)
-    {
-        $res = $this->request('profiles/'.$pid.'/cards');
-        
-        return $res['card'];
-    }
-    
-    /**
-     * Update card in a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/update-card-profile/
-     * 
-     * @param string $pid Profile Id
-     * @param int $cid Card Id
-     * @param array $data Card data
-     * @return TRUE
-     */
-    public function updateCard($pid, $cid, $data)
-    {
-        $this->request('profiles/'.$pid.'/cards/'.$cid, array('card' => $data), 'PUT');
-        
-        return true;
-    }
-    
-    /**
-     * Delete card from a profile
-     * @link http://developer.beanstream.com/documentation/tokenize-payments/delete-card-profile/
-     * 
-     * @param string $pid Profile Id
-     * @param int $cid Card Id
-     * @return TRUE
-     */
-    public function deleteCard($pid, $cid)
-    {
-        $this->request('profiles/'.$pid.'/cards/'.$cid, null, 'DELETE');
-        
-        return true;
-    }
-    
-    /**
-     * Search transactions
-     * @link http://developer.beanstream.com/documentation/analyze-payments/search-specific-criteria/
-     * 
-     * @param array[optional] $data Filtering data, the default values
-     *  name: 'Search'
-     *  start_date: now - 1 day OR end_date - 1 day
-     *  end_date: start_date + 1 day
-     *  start_row: 1
-     *  end_row: start_row + 9
-     *  i.e. by default function returns first 10 transactions for the past 24h
-     * @return array Transactions found
-     */
-    public function search($data = array())
-    {
-        // compose default values for date filter
-        $start_dt = new \DateTime(isset($data['end_date']) ? $data['end_date'] : null);
-        $start_dt->modify('-1 day +1 second');
-        $start_date = $start_dt->format('c');
-		
-        $end_dt = new \DateTime(isset($data['start_date']) ? $data['start_date'] : $start_date);
-        $end_dt->modify('+1 day');
-        $end_date = $end_dt->format('c');
-        
-        $res = $this->request('reports', $data + array(
-            'name' => 'Search',
-            'start_date' => $start_date,
-            'end_date' => $end_date,
-            'start_row' => 1,
-            'end_row' => 9 + (isset($data['start_row']) ? $data['start_row'] : 1),
-        ), 'POST');
-        
-        return $res['records'];
-    }
 }
