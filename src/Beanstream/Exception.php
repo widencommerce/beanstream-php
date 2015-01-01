06c75b94f0452e6545c2f80c61cd34f738b7a6a4
Kevin Saliba <ksaliba@beanstream.com>
2015-01-01 10:10:24 -0800

    added extended exception classes

--- src/Beanstream/Exception.php
+++ src/Beanstream/Exception.php
@@ -12,11 +12,46 @@ namespace Beanstream;
  * Negative error codes corresponde to those of cURL
  * @link http://curl.haxx.se/libcurl/c/libcurl-errors.html
  * 
- * @author Pavel Kulbakin <p.kulbakin@gmail.com>
+ * @author Kevin Saliba
  */
-class Exception extends \Exception
-{
+class Exception extends \Exception {
 	
+	protected $_code;
+	protected $_message;
+	
+	
+	public function __construct($message, $code = 0) {
+		
+		$this->_message = $message;
+		$this->_code = $code;
+		
+		//echo $this->__toString();
+		
+		parent::__construct($this->_message, $this->_code);
+		
+		
+		
+	}
+
+
+	
+	
+}
+
+
+
+class ConfigurationException extends Exception {}
+
+class ConnectorException extends Exception {}
+
+class ApiException extends Exception {
+	
+	public function __construct($message, $code = 0) {
+		
+		echo 'Api Exception: Code: '.$code.'; Message: \''.$message.'\'';
+		die();
+		
+	}
 	
 	
 }
